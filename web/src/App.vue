3cbd07236d40d2fa35082736a0447f8afc8e497b
wurifu <rifuwu123@gmail.com>
2022-07-14 23:55:37 +0800

    lesson1

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <h1>hello vue</h1>
+    <div>bot昵称: {{ bot_name }}</div>
+    <div>bot战力: {{ bot_rating }}</div>
   </nav>
   <router-view/>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar";
-import "bootstrap/dist/"
+import $ from 'jquery';
+import { ref } from 'vue';
+/*import NavBar from "@/co/!*mponents/NavBar";
+import "bootstrap/dist/"*!/*/
+
+
+export default{
+  name: "App",
+
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getBotInfo/",
+      type: "get",
+      success: resp => {
+        /*console.log(resp)*/
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    })
+
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
 </script>
 
 
 <style>
+
+body {
+  background-image: url("@/assets/background.png");
+  background-size: cover;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
